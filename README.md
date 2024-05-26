79db074ab1119299d7a32c81dc9e89469f0997ca
Slavo Vojacek <public@slavovojacek.com>
2024-05-26 09:17:30 +0100

    V2 (#1)

--- README.md
+++ README.md
@@ -1,37 +1,29 @@
-# cloudstd
+<p align="center">
+  <img src="https://raw.githubusercontent.com/thames-technology/apigen/main/.github/assets/apigen-cover.png" alt="API Gen Logo" />
+</p>
 
-Generate Protobuf service definitions conforming to popular design guides:
+<p align="right">
+  <i>If you use this repo, star it ✨</i>
+</p>
 
-- [Buf Style Guide](https://docs.buf.build/best-practices/style-guide)
-- [Uber Protobuf Style Guide V2](https://github.com/uber/prototool/blob/dev/style/README.md)
-- [Google Cloud API Design Guide](https://cloud.google.com/apis/design)
+---
 
-## Installation
+<h2 align="center">Generate best-practice Protobuf APIs following design patterns</h2>
 
-```sh
-go install github.com/slavovojacek/cloudstd@latest
-```
+<p align="center">
+  Inspired by <a href="https://www.oreilly.com/library/view/api-design-patterns/9781617295850/" target="_blank">API Design Patterns</a>
+</p>
 
-## Usage
+---
 
-### Modern, simple style
-
-This style is based on [Buf Style Guide](https://docs.buf.build/best-practices/style-guide) and [Uber Protobuf Style Guide V2](https://github.com/uber/prototool/blob/dev/style/README.md).
-
-```sh
-proto> cloudstd proto --package "acme.shelf.v1" --resource "shelf,shelves"
-proto> cloudstd proto --package "acme.book.v1" --resource "book" --parent "shelf,shelves"
-```
-
-The generated code can be found [in the docs](docs/examples/proto/modern/README.md).
-
-### Google style
-
-This style is based on the [Google Cloud API Design Guide](https://cloud.google.com/apis/design).
+## Install
 
 ```sh
-proto> cloudstd proto --package "acme.shelf.v1" --resource "shelf,shelves" --google
-proto> cloudstd proto --package "acme.book.v1" --resource "book" --parent "shelf,shelves" --google
+# TODO
 ```
 
-The generated code can be found [in the docs](docs/examples/proto/google/README.md).
+## Getting started
+
+```sh
+apigen proto -r book -pkg bookservice.v1alpha1
+```
--- main.go
+++ main.go
@@ -1,30 +1,83 @@
-/*
-Copyright © 2022 Slavo Vojacek <public@slavovojacek.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
 package main
 
 import (
-	"github.com/slavovojacek/cloudstd/cmd"
+	"log"
+	"os"
+
+	"github.com/thames-technology/apigen/internal/template"
+	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	cmd.Execute()
+	app := &cli.App{
+		Name:  "apigen",
+		Usage: "Generate best-practice Protobuf APIs following design patterns",
+		Commands: []*cli.Command{
+			{
+				Name: "proto",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "resource",
+						Aliases:  []string{"r"},
+						Usage:    "The name of the resource, e.g. book",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "resources",
+						Aliases: []string{"rs"},
+						Usage:   "The plural name of the resource, e.g. books",
+					},
+					&cli.StringFlag{
+						Name:    "parent",
+						Aliases: []string{"p"},
+						Usage:   "The name of the parent resource, e.g. author",
+					},
+					&cli.StringFlag{
+						Name:    "parents",
+						Aliases: []string{"ps"},
+						Usage:   "The plural name of the parent resource, e.g. authors",
+					},
+					&cli.StringFlag{
+						Name:     "package",
+						Aliases:  []string{"pkg"},
+						Usage:    "The package name, e.g. bookservice.v1alpha1",
+						Required: true,
+					},
+					&cli.StringFlag{
+						Name:    "out-dir",
+						Aliases: []string{"o"},
+						Usage:   "The directory to write the proto files to",
+						Value:   "proto",
+					},
+				},
+				Action: func(c *cli.Context) error {
+					resource := c.String("resource")
+					resources := c.String("resources")
+					if resources == "" {
+						resources = resource + "s"
+					}
+
+					parent := c.String("parent")
+					parents := c.String("parents")
+					if parent != "" && parents == "" {
+						parents = parent + "s"
+					}
+
+					data := &template.ProtoData{
+						Resource:  resource,
+						Resources: resources,
+						Parent:    parent,
+						Parents:   parents,
+						Package:   c.String("package"),
+					}
+
+					return template.Write(data, c.String("out-dir"))
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
